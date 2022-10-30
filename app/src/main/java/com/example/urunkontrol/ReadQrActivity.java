cb29799c27a52e1b71b7e6d2a23ca19edebdcdfc
Emre Kilcioglu <kilciogluemre05@gmail.com>
2022-10-30 23:51:40 +0300

    Temel İşçi Tasarımı yapıldı

---
+++ app/src/main/java/com/example/urunkontrol/ReadQrActivity.java
@@ -0,0 +1,14 @@
+package com.example.urunkontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadQrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_qr);
+    }
+}
---
+++ app/src/main/java/com/example/urunkontrol/employee/EmployeeChoiceActivity.java
@@ -0,0 +1,16 @@
+package com.example.urunkontrol.employee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.urunkontrol.R;
+
+public class EmployeeChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_choice);
+    }
+}
