1a947fbff59a690bf2615f66989280229e9f3bc6
Emre Kilcioglu <kilciogluemre05@gmail.com>
2022-11-01 22:34:05 +0300

    drawver öncesi kayıt

--- app/src/main/java/com/example/urunkontrol/ProductInputActivity.java
+++ app/src/main/java/com/example/urunkontrol/ProductInputActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ReadQrActivity extends AppCompatActivity {
+public class ProductInputActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_read_qr);
+        setContentView(R.layout.activity_product_input);
     }
 }
