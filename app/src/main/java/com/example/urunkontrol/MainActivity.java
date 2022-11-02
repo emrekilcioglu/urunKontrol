07f04fbf074d61b19e4120a369fc3ee6e5d3064b
Emre Kilcioglu <kilciogluemre05@gmail.com>
2022-11-02 23:36:47 +0300

    Görsel Nesnelerimizi Java Sınıfına Bağlamaya başladık

--- app/src/main/java/com/example/urunkontrol/MainActivity.java
+++ app/src/main/java/com/example/urunkontrol/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.urunkontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.urunkontrol.employee.EmployeeMainPageActivity;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonLogin;
+    private TextInputEditText editTextName,editTextPassword;
+    private Intent intent;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        editTextName = findViewById(R.id.edtitTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        intent = new Intent(MainActivity.this, EmployeeMainPageActivity.class);
+        buttonLogin.setOnClickListener(view -> {
+            startActivity(intent);
+
+        });
     }
 }
--- app/src/main/java/com/example/urunkontrol/employee/EmployeeMainPageActivity.java
+++ app/src/main/java/com/example/urunkontrol/employee/EmployeeMainPageActivity.java
@@ -1,16 +1,34 @@
 package com.example.urunkontrol.employee;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.example.urunkontrol.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class EmployeeMainPageActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayoutEmp;
+    private Toolbar toolbarEmp;
+    private NavigationView navViewEmp;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_main_page);
+        drawerLayoutEmp = findViewById(R.id.drawerLayoutEmp);
+        toolbarEmp = findViewById(R.id.toolbarEmp);
+        navViewEmp = findViewById(R.id.navViewEmp);
+        toolbarEmp.setTitle(R.string.toolbar_emp_main_page);
+        setSupportActionBar(toolbarEmp);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayoutEmp,toolbarEmp,0,0);
+        drawerLayoutEmp.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
